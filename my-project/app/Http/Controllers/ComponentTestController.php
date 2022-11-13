3e4682823ba5c81e5c16866ebe3a0871a67187fd
KogoAraki <elle_men@yahoo.co.jp>
2022-11-13 18:02:33 +0900

    slot attribute merge

--- my-project/app/Http/Controllers/ComponentTestController.php
+++ my-project/app/Http/Controllers/ComponentTestController.php
@@ -8,7 +8,8 @@ class ComponentTestController extends Controller
 {
     //
     public function showComponent1() {
-        return view('tests.component-test1');
+        $message = 'メッセージだよ。';
+        return view('tests.component-test1',compact('message'));
     }
     public function showComponent2() {
         return view('tests.component-test2');
--- my-project/resources/views/tests/component-test1.blade.php
+++ my-project/resources/views/tests/component-test1.blade.php
@@ -1,5 +1,7 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー1</x-slot>
     コンポーネント１
-    <x-tests.card title="タイトル" content="本文"/>
+    <x-tests.card title="タイトル" content="本文" :message="$message"/>
+    <x-tests.card title="タイトル" />
+    <x-tests.card title="cssを変更したい" class="bg-red-300"/>
 </x-tests.app>
