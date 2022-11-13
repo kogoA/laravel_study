dfe64f48273a96b29b55408503eea1c9a11f1c56
KogoAraki <elle_men@yahoo.co.jp>
2022-11-13 14:21:39 +0900

    表示ok

--- my-project/resources/views/layouts/guest.blade.php
+++ my-project/resources/views/layouts/guest.blade.php
@@ -3,19 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @routes
-        @viteReactRefresh
-        <!-- @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"]) -->
-        @inertiaHead
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        @inertia
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            {{ $slot }}
+        </div>
     </body>
 </html>
