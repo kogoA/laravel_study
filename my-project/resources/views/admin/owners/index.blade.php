a21715ad609aac425348e42f24d5935bac047f6a
KogoAraki <elle_men@yahoo.co.jp>
2022-12-02 13:48:40 +0900

    管理者側完了

--- my-project/resources/views/admin/owners/index.blade.php
+++ my-project/resources/views/admin/owners/index.blade.php
@@ -8,9 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="md:p-6 bg-white border-b border-gray-200">
                        <section class="text-gray-600 body-font">
-                        <div class="container px-5 mx-auto">
+                        <div class="container md:px-5 mx-auto">
                             <x-flash-message status="session('status')" />
                           <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                             <div class='flex justify-end mb-4'>
@@ -19,9 +19,9 @@
                             <table class="table-auto w-full text-left whitespace-no-wrap">
                               <thead>
                                 <tr>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
-                                  <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
+                                  <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
+                                  <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
+                                  <th class="md:px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
                                   <th class="title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                                   <th class="title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                                 </tr>
@@ -29,9 +29,9 @@
                               <tbody>
                                 @foreach ($owners as $owner)
                                 <tr>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">{{ $owner->name }} </td>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">{{ $owner->email }}</td>
-                                  <td class="border-t-2 border-gray-200 px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
+                                  <td class="border-t-2 border-gray-200 md:px-4 py-3">{{ $owner->name }} </td>
+                                  <td class="border-t-2 border-gray-200 md:px-4 py-3">{{ $owner->email }}</td>
+                                  <td class="border-t-2 border-gray-200 md:px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
                                   <td class="border-t-2 border-gray-200 text-center">
                                     <button onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id ])}}'" class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded">編集する</button>
                                   </td>
