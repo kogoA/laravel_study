5267b3716e14e24cd8351edf8266faa4398228d3
KogoAraki <elle_men@yahoo.co.jp>
2023-01-28 19:31:47 +0900

    cart index メソッド実装途中

--- my-project/app/Http/Controllers/User/CartController.php
+++ my-project/app/Http/Controllers/User/CartController.php
@@ -5,10 +5,26 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
+
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach($products as $product){
+            $totalPrice += $product->price * $product->pivot->quantity;
+            echo $totalPrice." " ;
+        }
+
+        return view('user.cart.index',compact('products', 'totalPrice'));
+    }
+
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)->where('user_id', Auth::id())->first();
@@ -24,5 +40,7 @@ class CartController extends Controller
                 'quantity' => $request->quantity
             ]);
         }
+
+        return redirect()->route('user.cart.index');
     }
 }
