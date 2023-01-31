d62a169bd8c89be75f09797782d3223037c048b7
KogoAraki <elle_men@yahoo.co.jp>
2023-01-31 13:05:16 +0900

    pagination設定完了

--- my-project/resources/views/user/index.blade.php
+++ my-project/resources/views/user/index.blade.php
@@ -17,7 +17,14 @@
                                 <option value="{{ \Constant::SORT_ORDER['older']}}" @if(\Request::get('sort') === \Constant::SORT_ORDER['older']) selected @endif>古い順</option>
                             </select>
                         </div>
-                        <div>表示件数</div>
+                        <div>
+                            <span class="text-sm">表示件数</span><br>
+                            <select id="pagination" name="pagination">
+                                <option value="20" @if(\Request::get('pagination') === '20') selected @endif>20件</option>
+                                <option value="50" @if(\Request::get('pagination') === '50') selected @endif>50件</option>
+                                <option value="100" @if(\Request::get('pagination') === '100') selected @endif>100件</option>
+                            </select>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -47,7 +54,10 @@
                         </div>
                         @endforeach
                     </div>
-
+                    {{ $products->appends([
+                        'sort'       => \Request::get('sort'),
+                        'pagenation' => \Request::get('paginationo')
+                    ])->links()}}
                 </div>
             </div>
         </div>
@@ -55,6 +65,11 @@
     <script>
         const select = document.getElementById('sort')
         select.addEventListener('change', function(){
+            this.form.submit()
+        })
+
+        const paginate = document.getElementById('pagination')
+        paginate.addEventListener('change', function(){
             this.form.submit()
         })
     </script>
